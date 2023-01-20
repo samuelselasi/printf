dd3e6dc22bd8046ea875617f9f5e86dc104925b5
NewGit1999 <yasminebenali32@gmail.com>
2023-01-20 15:33:59 -0800

    short-short int to hex

---
+++ hhitox.c
@@ -0,0 +1,38 @@
+#include "main.h"
+
+/**
+ * hhitox - convert to hex(short short)
+ * @list: args
+ * Return: string
+ */
+
+char *hhitox(va_list list)
+{
+	unsigned char digits, digitTest;
+	int count, i;
+	char *result;
+
+	digits = (unsigned char) va_arg(list, unsigned int);
+	count = 0;
+	digitTest = digits;
+	while (digitTest > 0)
+	{
+		digitTest /= 16;
+		count++;
+	}
+
+	result = malloc(sizeof(char) * count + 1);
+	if (result == NULL)
+		return (NULL);
+	digitTest = digits;
+	for (i = count - 1; i >= 0; i--)
+	{
+		if (digitTest % 16 > 9)
+			result[i] = (digitTest % 16) + 87;
+		else
+			result[i] = (digitTest % 16) + '0';
+		digitTest /= 16;
+	}
+	result[count] = '\0';
+	return (result);
+}
