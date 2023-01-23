586d4ff82068372e3aa30d833c5bd6665f32df8b
samuelselasi <samelsekasi@gmail.com>
2023-01-23 15:34:26 -0800

    add print integer

---
+++ print_int.c
@@ -0,0 +1,46 @@
+#include "main.h"
+
+/**
+ * print_int - print integer i or d
+ * @types: type args
+ * @buffer: buffer
+ * @flags: find active flags
+ * @width: width
+ * @precision: precision
+ * @size: size
+ *
+ * Return: int
+ */
+
+int print_int(va_list types, char buffer[],
+	int flags, int width, int precision, int size)
+{
+	int i = BUFF_SIZE - 2;
+	int is_negative = 0;
+	long int n = va_arg(types, long int);
+	unsigned long int num;
+
+	n = convert_size_number(n, size);
+
+	if (n == 0)
+		buffer[i--] = '0';
+
+	buffer[BUFF_SIZE - 1] = '\0';
+	num = (unsigned long int)n;
+
+	if (n < 0)
+	{
+		num = (unsigned long int)((-1) * n);
+		is_negative = 1;
+	}
+
+	while (num > 0)
+	{
+		buffer[i--] = (num % 10) + '0';
+		num /= 10;
+	}
+
+	i++;
+
+	return (write_number(is_negative, i, buffer, flags, width, precision, size));
+}
