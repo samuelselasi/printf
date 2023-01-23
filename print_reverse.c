3a377a8eca8f3d752360a133e8585412d3af7a34
NewGit1999 <yasminebenali32@gmail.com>
2023-01-23 15:10:11 -0800

    add print functions

---
+++ print_reverse.c
@@ -0,0 +1,45 @@
+#include "main.h"
+
+/**
+ * print_reverse - print string in reverse
+ * @types: type args
+ * @buffer: buffer
+ * @flags: find active flags
+ * @width: width
+ * @precision: precision
+ * @size: size
+ *
+ * Return: int
+ */
+
+int print_reverse(va_list types, char buffer[],
+	int flags, int width, int precision, int size)
+{
+	char *str;
+	int i, count = 0;
+
+	UNUSED(buffer);
+	UNUSED(flags);
+	UNUSED(width);
+	UNUSED(size);
+
+	str = va_arg(types, char *);
+
+	if (str == NULL)
+	{
+		UNUSED(precision);
+
+		str = ")Null(";
+	}
+	for (i = 0; str[i]; i++)
+		;
+
+	for (i = i - 1; i >= 0; i--)
+	{
+		char z = str[i];
+
+		write(1, &z, 1);
+		count++;
+	}
+	return (count);
+}
---
+++ print_rot13.c
@@ -0,0 +1,54 @@
+#include "main.h"
+
+/**
+ * print_rot13string - print Rot13 encrypted string
+ * @types: type args
+ * @buffer: buffer
+ * @flags: find active flags
+ * @width: width
+ * @precision: precision
+ * @size: size
+ *
+ * Return: int
+ */
+
+int print_rot13string(va_list types, char buffer[],
+	int flags, int width, int precision, int size)
+{
+	char x;
+	char *str;
+	unsigned int i, j;
+	int count = 0;
+	char in[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char out[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+
+	str = va_arg(types, char *);
+	UNUSED(buffer);
+	UNUSED(flags);
+	UNUSED(width);
+	UNUSED(precision);
+	UNUSED(size);
+
+	if (str == NULL)
+		str = "(AHYY)";
+	for (i = 0; str[i]; i++)
+	{
+		for (j = 0; in[j]; j++)
+		{
+			if (in[j] == str[i])
+			{
+				x = out[j];
+				write(1, &x, 1);
+				count++;
+				break;
+			}
+		}
+		if (!in[j])
+		{
+			x = str[i];
+			write(1, &x, 1);
+			count++;
+		}
+	}
+	return (count);
+}
